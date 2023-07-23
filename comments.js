19ff4719c87028b39ef44d8f8b86d34aff87954d
Wilson Ngunjiri <115584322+Toonswill@users.noreply.github.com>
2023-07-23 19:44:35 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,29 @@
+// create web server
+// create web server
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+    console.log('request was made: ' + req.url);
+    if (req.url === '/home' || req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/contact') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if (req.url === '/api/comments') {
+        var comments = [
+            { name: 'John', age: 20 },
+            { name: 'Jane', age: 30 }
+        ];
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(comments));
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
+});
+
+server.listen(3000, function () {
+    console.log('Server listening on port 3000');
+});
